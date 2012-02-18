926cd7b9fe89c251de62c22522e3de746af421c3
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2012-02-18 17:15:11 +0900

    added project url

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject org.clojars.taka2ru/resource-scope "0.0.1-SNAPSHOT"
   :description "Resource Scope looking ahead"
+  :url "https://github.com/t2ru/resource-scope"
   :dependencies [[org.clojure/clojure "1.3.0"]])
 
