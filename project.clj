e023d1c6341adaffa59b4adf72410b46a1b03520
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2012-02-18 20:54:15 +0900

    0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.taka2ru/resource-scope "0.0.1-SNAPSHOT"
+(defproject org.clojars.taka2ru/resource-scope "0.0.1"
   :description "Resource Scope looking ahead"
   :url "https://github.com/t2ru/resource-scope"
   :dependencies [[org.clojure/clojure "1.3.0"]])
